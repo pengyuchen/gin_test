725803d0aae039603a3f2e89757ae01fce8aa4c0
Huangjr <jr@nlplab.cc>
2020-01-28 13:03:59 +0800

    post bug

--- db.sql
+++ db.sql
@@ -1,17 +1,17 @@
 -- MySql 
 
--- create database if not exists gin_exer;
--- use gin_exer;
--- create table `person` (
--- 	`id` int(11) not null auto_increment primary key,
--- 	`firstname` varchar(40) not null default "",
--- 	`lastname`  varchar(40) not null default ""
--- ) engine=innodb default charset=utf8;
+create database if not exists gin_exer;
+use gin_exer;
+create table `person` (
+	`id` int(11) not null auto_increment primary key,
+	`firstName` varchar(40) not null default "",
+	`lastName`  varchar(40) not null default ""
+) engine=innodb default charset=utf8;
 
 
-CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) DEFAULT NULL,
-  `telephone` varchar(12) DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+-- CREATE TABLE `users` (
+--   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+--   `name` varchar(64) DEFAULT NULL,
+--   `telephone` varchar(12) DEFAULT '',
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
